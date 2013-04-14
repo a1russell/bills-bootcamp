6f7e43be141cf90f11bcbb4f43330b79a89e07d1
Adam Russell <adamlr6+pub@gmail.com>
2013-04-14 01:40:28 -0500

    Add a simple main method.

--- rectangles/src/main/java/problem2/Main.java
+++ rectangles/src/main/java/problem2/Main.java
@@ -3,6 +3,17 @@ package problem2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Point topLeft = new Point(0, 2);
+        Point topRight = new Point(2, 2);
+        Point bottomRight = new Point(2, 0);
+
+        Rectangle rectangle = new Rectangle(topLeft, topRight, bottomRight);
+
+        Point point = new Point(1, 1);
+        if (rectangle.contains(point)) {
+            System.out.println("The rectangle contains the point.");
+        } else {
+            System.out.println("The rectangle does not contain the point.");
+        }
     }
 }
