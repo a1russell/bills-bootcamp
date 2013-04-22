1f53224339305e4cee118c85c4d3fee9cdc42133
Adam Russell <adamlr6+pub@gmail.com>
2013-04-21 21:09:07 -0500

    Add Main class.

---
+++ unitconversion/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] argv) {
+        System.out.println("3 tsp. is 1 tbsp.");
+    }
+}
---
+++ unitconversion/src/test/java/MainIT.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertFalse;
+
+public class MainIT {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testMain() throws Exception {
+        String[] argv = {};
+        Main.main(argv);
+        assertFalse(out.toString().isEmpty());
+    }
+}
