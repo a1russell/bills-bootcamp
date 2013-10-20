4271e505d3fc6454d4e168279084f875a79a9738
Adam Russell <adamlr6+pub@gmail.com>
2013-10-20 00:33:51 -0500

    Generate centered diamonds of size greater than 1.

--- centereddiamond/src/main/scala/CenteredDiamond.scala
+++ centereddiamond/src/main/scala/CenteredDiamond.scala
@@ -3,6 +3,20 @@ object CenteredDiamond extends (Int => String) {
     if (size % 2 == 0) {
       throw new IllegalArgumentException("Size must be odd.")
     }
-    "*"
+    apply(size, 1)
+  }
+
+  private def apply(size: Int, current: Int): String = {
+    val paddingSize = (size - current) / 2
+    val padding = " " * paddingSize
+    val stars = "*" * current
+    val currentLine = padding + stars + "\n"
+    var result = ""
+    result += currentLine
+    if (current < size) {
+      result += apply(size, current + 2)
+      result += currentLine
+    }
+    result
   }
 }
--- centereddiamond/src/test/scala/CenteredDiamondSpec.scala
+++ centereddiamond/src/test/scala/CenteredDiamondSpec.scala
@@ -7,13 +7,24 @@ class CenteredDiamondSpec extends Specification {
 
   "A centered diamond of size 1" should {
     "consist of 1 star" in {
-      CenteredDiamond(1) must be_==("*")
+      CenteredDiamond(1) must be_==("*\n")
     }
   }
 
   "A centered diamond of size 2" should {
     "not be valid" in {
       CenteredDiamond(2) must throwAn[IllegalArgumentException]
+    }
+  }
+
+  "A centered diamond of size 5" should {
+    "look like a centered diamond, 5 stars wide" in {
+      CenteredDiamond(5) must be_==("""|  *
+                                       | ***
+                                       |*****
+                                       | ***
+                                       |  *
+                                       |""".stripMargin)
     }
   }
 }
