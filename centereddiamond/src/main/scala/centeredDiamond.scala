a4c97b0d2b84306099279d0d932e1a6287d94257
Adam Russell <adamlr6+pub@gmail.com>
2013-10-29 10:52:26 -0500

    Remove private apply method in favor of nested go function.

--- centereddiamond/src/main/scala/centeredDiamond.scala
+++ centereddiamond/src/main/scala/centeredDiamond.scala
@@ -1,20 +1,20 @@
 object centeredDiamond extends (Int => String) {
-  def apply(size: Int): String = {
-    if (size % 2 == 0) {
-      throw new IllegalArgumentException("Size must be odd.")
-    }
-    apply(size, 1)
-  }
 
-  private def apply(size: Int, current: Int): String = {
-    val paddingSize = (size - current) / 2
-    val padding = " " * paddingSize
-    val stars = "*" * current
-    val currentLine = padding + stars + "\n"
-    if (current < size) {
-      currentLine + apply(size, current + 2) + currentLine
-    } else {
-      currentLine
+  def apply(size: Int): String = {
+
+    def go(current: Int): String = {
+      val paddingSize = (size - current) / 2
+      val padding = " " * paddingSize
+      val stars = "*" * current
+      val currentLine = padding + stars + "\n"
+      if (current < size) {
+        currentLine + go(current + 2) + currentLine
+      } else {
+        currentLine
+      }
     }
+
+    require(size % 2 != 0, "Size must be odd.")
+    go(1)
   }
 }
